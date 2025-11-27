b45775a2c2a2c5d14922e2fa1868f27d2ef566a9
luis mackiewicz <luismiguelpix@gmail.com>
2025-11-27 00:23:26 -0300

    remove(resources): remove resources to remake this

--- apps/backend/src/supliers/dto/update-suplier.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSuplierDto } from './create-suplier.dto';
-
-export class UpdateSuplierDto extends PartialType(CreateSuplierDto) {}
--- apps/backend/src/supliers/supliers.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SupliersService } from './supliers.service';
-import { SupliersController } from './supliers.controller';
-
-@Module({
-  controllers: [SupliersController],
-  providers: [SupliersService],
-})
-export class SupliersModule {}
