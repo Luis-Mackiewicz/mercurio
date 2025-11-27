8f87251007465be152519953ac31bd0c9826ba6d
luis mackiewicz <luismiguelpix@gmail.com>
2025-11-27 19:10:01 -0300

    feat(main): add new messages

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,16 @@
+// main.ts (Corrigido)
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3333);
+
+  const port = process.env.PORT ?? 3333;
+
+  await app.listen(port);
+
+  console.log(` Mercurio ERP iniciada e rodando na porta ${port}`);
 }
+
 void bootstrap();
