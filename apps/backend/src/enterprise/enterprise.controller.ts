98ba94fb2aa8bfd5cac6ab29d65b9bf4d2e4ae42
luis mackiewicz <luismiguelpix@gmail.com>
2025-11-27 18:21:44 -0300

    fix(enterprise): resolving imports

--- apps/backend/src/enterprise/enterprise.controller.ts
+++ apps/backend/src/enterprise/enterprise.controller.ts
@@ -1,14 +1,22 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { EnterpriseService } from './enterprise.service';
-import { CreateEnterpriseDto } from './dto/create-enterprise.dto';
-import { UpdateEnterpriseDto } from './dto/update-enterprise.dto';
+import type { CreateEnterpriseDTO } from './dto/create-enterprise.dto';
+import type { UpdateEnterpriseDTO } from './dto/update-enterprise.dto';
 
 @Controller('enterprise')
 export class EnterpriseController {
   constructor(private readonly enterpriseService: EnterpriseService) {}
 
   @Post()
-  create(@Body() createEnterpriseDto: CreateEnterpriseDto) {
+  create(@Body() createEnterpriseDto: CreateEnterpriseDTO) {
     return this.enterpriseService.create(createEnterpriseDto);
   }
 
@@ -23,7 +31,10 @@ export class EnterpriseController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateEnterpriseDto: UpdateEnterpriseDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateEnterpriseDto: UpdateEnterpriseDTO,
+  ) {
     return this.enterpriseService.update(+id, updateEnterpriseDto);
   }
 
