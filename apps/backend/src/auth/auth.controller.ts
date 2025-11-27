bcb09be86f64a785d35e04546671182fa6b8c654
luis mackiewicz <luismiguelpix@gmail.com>
2025-11-27 18:52:27 -0300

    feat(auth): create decorators and status

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Post, UseGuards, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Req,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import type { Request } from 'express';
 import type { JwtRefreshRequest } from './types/refresh-request.types';
@@ -10,12 +18,18 @@ import type { LoginUserDto } from './dto/login-user.dto';
 
 import { ZodValidationPipe } from '../common/pipes/zod-validation.pipes';
 import { JwtRefreshGuard } from './guards/jwt.guard';
+import { Public } from '../common/decorators/public.decorators';
+import { JwtAuthGuard } from './guards/jwt.auth.guard';
+
+type JwtAuthRequest = Request & { user: { sub: string } };
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('register')
+  @HttpCode(HttpStatus.CREATED)
   async register(
     @Body(new ZodValidationPipe(createUserSchema))
     dto: CreateUserDto,
@@ -23,7 +37,9 @@ export class AuthController {
     return this.authService.register(dto);
   }
 
+  @Public()
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   async login(
     @Body(new ZodValidationPipe(loginUserSchema))
     dto: LoginUserDto,
@@ -31,9 +47,18 @@ export class AuthController {
     return this.authService.login(dto);
   }
 
+  @Public()
   @UseGuards(JwtRefreshGuard)
   @Post('refresh')
+  @HttpCode(HttpStatus.OK)
   async refresh(@Req() req: JwtRefreshRequest) {
     return this.authService.refreshTokens(req.user.sub, req.refreshToken);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  async logout(@Req() req: JwtAuthRequest) {
+    return this.authService.logout(req.user.sub);
   }
 }
