1205268d9da018822e54f604b6a5b84be2b4ba85
luis mackiewicz <luismiguelpix@gmail.com>
2025-11-27 19:10:27 -0300

    fix(module): resolve prlem with library

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,16 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controler';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    PrismaModule,
-    AuthModule,
-    UsersModule,
-  ],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
