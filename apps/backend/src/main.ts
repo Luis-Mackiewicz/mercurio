2b78a69264037ffa07b0aadd076adfef81944740
luis mackiewicz <luismiguelpix@gmail.com>
2025-11-27 20:09:09 -0300

    refactor(main): refactor port

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT ?? 3333;
+  const port = process.env.PORT ?? 3332;
 
   await app.listen(port);
 
